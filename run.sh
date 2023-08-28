788bde21f8208350386a1d9d68a1645869a2bdb4
HongLiu <32379010+ccarliu@users.noreply.github.com>
2023-08-28 20:55:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-# CUDA_VISIBLE_DEVICES=0 python train_dme.py --gpu 0 --batch_size 2 --name test --weight_decay 0.0005 --lr 0.005 --patch_size 48
-CUDA_VISIBLE_DEVICES=0 python test_dme.py --gpu 0 --ck_name dme_ck2.t7 --batch_size 2 --name test --patch_size 48
+# for dme dataset:
+
+CUDA_VISIBLE_DEVICES=0 python train_dme.py --gpu 0 --root yourdatapath --batch_size 4 --name test --weight_decay 0.0005 --lr 0.005 --patch_size 48
+CUDA_VISIBLE_DEVICES=0 python test_dme.py --gpu 0 --root yourdatapath --ck_name dme_ck2.t7 --batch_size 4 --name test --patch_size 48
