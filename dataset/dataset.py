f3a796cd1f700f0f7575d58f1ff8eb1ce1c14808
HongLiu <32379010+ccarliu@users.noreply.github.com>
2023-08-28 20:46:04 +0800

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -113,6 +113,320 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.data)
+
+class sd_oct_flatten_ori_align_doe(torch.utils.data.Dataset):
+    """ 
+        dataset for a2a_sd_oct.
+        the root should be "/data3/layer_segmentation/a2a_oct".
+        the train and valid sets are splited by the label.csv.
+    """
+    
+    def __init__(self, root, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None, inte_dis = False,step=30, test = False, asize = 224, patch_size = 48):
+        super(sd_oct_flatten_ori_align_doe, self).__init__()
+
+        self.data_path = []
+        self.data = []
+        self.seg = []
+        self.label_idx = []
+        self.name = []
+        self.step = step
+        self.test = test 
+        self.inte_dis = inte_dis
+        self.usage = usage
+        self.valid_fold = valid_fold
+        self.asize = asize
+        self.patch_size = patch_size
+
+        # load file path and split train set and valid set.
+        files = glob.glob(os.path.join(root, "*.mat"))
+        if self.usage == "train":
+            self.data_path = sorted(files)[5:]
+        else:
+            self.data_path = sorted(files)[:5]
+    
+        print("load " + self.usage + " " + str(len(self.data_path)))
+        self.load_data()
+        
+    def load_data(self):
+    
+        self.data = []
+        self.seg = []
+        self.label_idx = []
+        self.name = []
+        
+        maxi = 0
+        mini = 512
+        # load data and seg
+        for l in self.data_path:
+            try:
+                matInfo = scio.loadmat(l)
+            except:
+                print(l)
+                continue
+            
+            
+            cur_img = matInfo['images']            # size: (496, 768, 61)  -> (496, 512, 61)
+            cur_seg = matInfo['manualLayers1']     # original paper uses MJA's segmentation result.  size: (8, 768, 61) -> (8, 512, 61)
+
+            nansBscan = np.isnan(cur_seg).astype(int)  # size: 8x512x61
+            nansBscan = np.sum(nansBscan, axis=(0,1))  # size: 61
+
+            cidx = []
+            for ii,l1 in enumerate(nansBscan):
+                if l1 < 8*512:
+                    cidx.append(ii)
+            
+
+
+            ll = cidx[0]
+            cur_img = cur_img[:,:,ll:ll+41]
+            cur_seg = cur_seg.transpose(1,2,0)
+            z_indexes, y_indexes, x_indexes = np.nonzero(~np.isnan(cur_seg))
+            zmin, ymin, xmin = [max(0, int(np.min(arr))) for arr in (z_indexes, y_indexes, x_indexes)]
+            zmax, ymax, xmax = [int(np.max(arr) + 1) for arr in (z_indexes, y_indexes, x_indexes)]
+            
+            # random 1: for data augmentation
+            if self.usage == "train":
+                tempi = np.random.randint(0,10)
+                cur_seg = cur_seg[zmin+tempi:zmax, cidx, :]
+                cur_img = cur_img[:, zmin+tempi:zmax, :]
+            else:
+                tempi = 0
+                zmin = 128
+                zmax = 640
+                cur_seg = cur_seg[zmin+tempi:zmax, cidx, :]
+                cur_img = cur_img[:, zmin+tempi:zmax, :]
+            
+            
+                       
+            # random 2: for dataset augmentation
+            if self.usage == "train":
+                bv = np.random.randint(np.max([np.nanmax(cur_seg) - self.asize + 5, 0]), np.nanmin(cur_seg)-5)#int(np.nanmin(cur_seg) - 10)
+            else:
+                bv = int(np.nanmin(cur_seg) - 10)
+            
+            cur_img = cur_img[bv : bv + self.asize]
+            
+            cur_seg -= bv
+            
+            
+            temp = cidx[0]
+            cidx = [l-temp for l in cidx]
+            
+            if cur_seg.max() > maxi:
+                maxi = cur_seg.max()
+            if cur_seg.min() < mini:
+                mini = cur_seg.min()
+            
+
+            # normalization
+            mu = np.mean(cur_img, axis=0)
+            sigma = np.std(cur_img, axis=0)
+            cur_img = (cur_img - mu) / sigma
+            
+            # patch
+            step = self.step
+            for i in range((cur_img.shape[1] - self.patch_size) // step):
+                cur_idx = i*step
+                self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+                cur_idx = i*step
+                self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + self.patch_size,:])
+                self.seg.append((cur_seg).astype(np.float32)[cur_idx : cur_idx + self.patch_size,:,:])
+                self.label_idx.append(cidx)
+            cur_idx = zmax - zmin - tempi - self.patch_size
+            self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+            self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + self.patch_size,:])
+            self.label_idx.append(cidx)
+            self.seg.append((cur_seg).astype(np.float32)[cur_idx : cur_idx + self.patch_size,:,:])
+            
+        print("load " + self.usage + " " + str(len(self.data)))
+
+    def __getitem__(self, index):
+        patch = self.data[index]
+        name = self.name[index]
+        seg = self.seg[index]
+        cidx = self.label_idx[index]
+                
+        # data augmenation        
+        if self.usage == "train":
+            if np.random.random() > 0.7:
+                patch = patch[:, ::-1, :].copy()
+                seg = seg[::-1, :, :].copy()
+        
+        patch = torch.tensor(patch).unsqueeze(0)
+        
+        nan_mask = (np.isnan(seg).sum(-1) == 0).astype(np.float32)
+        seg[np.isnan(seg)] = 110
+        
+        layer_gt = getLayerLabels(torch.tensor(seg).permute(2,0,1), self.asize)
+        return patch, torch.tensor(seg).permute(2,0,1), layer_gt, name, cidx, nan_mask
+
+    def __len__(self):
+        return len(self.data)
+
+class sd_oct_flatten_ori_align_doe_lesion(torch.utils.data.Dataset):
+    """ 
+        dataset for a2a_sd_oct.
+        the root should be "/data3/layer_segmentation/a2a_oct".
+        the train and valid sets are splited by the label.csv.
+    """
+    
+    def __init__(self, root, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None, inte_dis = False,step=30, test = False, asize = 224, patch_size = 48):
+        super(sd_oct_flatten_ori_align_doe_lesion, self).__init__()
+
+        self.data_path = []
+        self.data = []
+        self.seg = []
+        self.label_idx = []
+        self.name = []
+        self.step = step
+        self.test = test 
+        self.inte_dis = inte_dis
+        self.usage = usage
+        self.valid_fold = valid_fold
+        self.asize = asize
+        self.patch_size = patch_size
+
+        # load file path and split train set and valid set.
+        files = glob.glob(os.path.join(root, "*.mat"))
+        if self.usage == "train":
+            self.data_path = sorted(files)[5:]
+        else:
+            self.data_path = sorted(files)[:5]
+    
+        self.load_data()
+        
+    def load_data(self):
+    
+        self.data = []
+        self.seg = []
+        self.label_idx = []
+        self.name = []
+        self.lesions = []
+        
+        maxi = 0
+        mini = 512
+        
+        # load data and seg
+        for l in self.data_path:
+            try:
+                matInfo = scio.loadmat(l)
+            except:
+                print(l)
+                continue
+           
+            
+            cur_img = matInfo['images']                # size: (496, 768, 61)  -> (496, 512, 61)
+            cur_seg = matInfo['manualLayers1']          # original paper uses MJA's segmentation result.  size: (8, 768, 61) -> (8, 512, 61)
+            cur_lesion = matInfo['manualFluid1']
+            
+            nansBscan = np.isnan(cur_seg).astype(int)  # size: 8x512x61
+            nansBscan = np.sum(nansBscan, axis=(0,1))  # size: 61
+
+            cidx = []
+            for ii,l1 in enumerate(nansBscan):
+                if l1 < 8*512:
+                    cidx.append(ii)
+
+
+            ll = cidx[0]
+
+            cur_img = cur_img[:,:,ll:ll+41]
+            cur_lesion = cur_lesion[:, :, ll:ll+41]
+
+            cur_seg = cur_seg.transpose(1,2,0)
+            z_indexes, y_indexes, x_indexes = np.nonzero(~np.isnan(cur_seg))
+            zmin, ymin, xmin = [max(0, int(np.min(arr))) for arr in (z_indexes, y_indexes, x_indexes)]
+            zmax, ymax, xmax = [int(np.max(arr) + 1) for arr in (z_indexes, y_indexes, x_indexes)]
+            
+            # random 1: for data augmentation
+            if self.usage == "train":
+                tempi = np.random.randint(0,self.patch_size // 2)
+            else:
+                tempi = 0
+            
+            cur_seg = cur_seg[zmin+tempi:zmax, cidx, :]
+            cur_img = cur_img[:, zmin+tempi:zmax, :]
+            cur_lesion = cur_lesion[:, zmin+tempi:zmax, :]
+            
+            
+            # random 2: for data augmentation
+            if self.usage == "train":
+                bv = np.random.randint(np.max([np.nanmax(cur_seg) - self.asize + 5, 0]), np.nanmin(cur_seg)-5)#int(np.nanmin(cur_seg) - 10)
+            else:
+                bv = int(np.nanmin(cur_seg) - 10)
+            
+            cur_img = cur_img[bv : bv + self.asize]
+            cur_lesion = cur_lesion[bv : bv + self.asize]
+            cur_lesion[cur_lesion > 0.5] = 1
+            
+            cur_seg -= bv            
+            cur_seg[cur_seg < 0] = 0  
+            cur_seg[cur_seg > self.asize] = self.asize
+            
+            
+            temp = cidx[0]
+            cidx = [l-temp for l in cidx]
+            
+            if cur_seg.max() > maxi:
+                maxi = cur_seg.max()
+            if cur_seg.min() < mini:
+                mini = cur_seg.min()
+            
+            # normalization
+            mu = np.mean(cur_img, axis=0)
+            sigma = np.std(cur_img, axis=0)
+            cur_img = (cur_img - mu) / sigma
+            
+            
+            # patch
+            step = self.step
+            for i in range((cur_img.shape[1] - self.patch_size) // step):
+                cur_idx = i*step
+                self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+                cur_idx = i*step
+                self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + self.patch_size,:])
+                self.seg.append((cur_seg).astype(np.float32)[cur_idx : cur_idx + self.patch_size,:,:])
+                self.label_idx.append(cidx)
+                self.lesions.append((cur_lesion).astype(np.float32)[:, cur_idx:cur_idx + self.patch_size, :])
+            cur_idx = zmax - zmin - tempi - self.patch_size
+            self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+            self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + self.patch_size,:])
+            self.label_idx.append(cidx)
+            self.seg.append((cur_seg).astype(np.float32)[cur_idx : cur_idx + self.patch_size,:,:])
+            self.lesions.append((cur_lesion).astype(np.float32)[:, cur_idx:cur_idx + self.patch_size, :])
+            
+        print("load " + self.usage + " " + str(len(self.data)))
+
+    def __getitem__(self, index):
+        patch = self.data[index]
+        name = self.name[index]
+        seg = self.seg[index]
+        cidx = self.label_idx[index]
+        clesion = self.lesions[index]
+        
+        # for data augmentation
+        if self.usage == "train":
+            if np.random.random() > 0.7:
+                patch = patch[:, ::-1, :].copy()
+                seg = seg[::-1, :, :].copy()
+                clesion = clesion[:, ::-1, :].copy()
+        
+        patch = torch.tensor(patch).unsqueeze(0)
+        clesion = torch.tensor(clesion).unsqueeze(0)
+        
+        nan_mask = (np.isnan(seg).sum(-1) == 0).astype(np.float32)
+        seg[np.isnan(seg)] = 110
+        clesion[np.isnan(clesion)] = 0
+        
+        layer_gt = getLayerLabels(torch.tensor(seg).permute(2,0,1), self.asize)
+        if self.usage == "train":
+            return patch, torch.tensor(seg).permute(2,0,1), layer_gt, clesion, cidx, nan_mask
+        else:
+            return patch, torch.tensor(seg).permute(2,0,1), layer_gt, name, cidx, nan_mask
+
+    def __len__(self):
+        return len(self.data)
         
 
 
