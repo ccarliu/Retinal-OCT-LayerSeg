8d3559720470dee0d5358e36b497bba5164cc615
HongLiu <32379010+ccarliu@users.noreply.github.com>
2023-08-28 20:48:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # CUDA_VISIBLE_DEVICES=0 python train_dme.py --gpu 0 --batch_size 2 --name test --weight_decay 0.0005 --lr 0.005 --patch_size 48
-CUDA_VISIBLE_DEVICES=0 python test_dme.py --gpu 0 --ck_name refinedgdweightx0_wdecay00001_lr001_p48_nopretrain_056_fold0_2.131%.t7 --batch_size 2 --name test --patch_size 48
+CUDA_VISIBLE_DEVICES=0 python test_dme.py --gpu 0 --ck_name dme_ck2.t7 --batch_size 2 --name test --patch_size 48
