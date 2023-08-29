a1aa75ad8971d1d4cfc091b979113d9382dff1ee
HongLiu <32379010+ccarliu@users.noreply.github.com>
2023-08-29 14:40:39 +0800

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,9 +1,10 @@
-
 import os
 import csv
 import glob
 import torch
 import torch.nn.functional as F
+# import torchvision.transforms as TF
+
 import random
 import torch.utils.data
 import scipy.io as scio
@@ -16,38 +17,46 @@ import scipy.ndimage
 from sklearn import preprocessing
 
 def getLayerLabels(surfaceLabels, height):
+    '''
+    for N surfaces, surface 0 in its exact location marks as 1, surface N-1 in its exact location marks as N.
+    region pixel between surface i and surface i+1 marks as i.
 
+    :param surfaceLabels: float tensor in size of (N,W) where N is the number of surfaces.
+            height: original image height
+    :return: layerLabels: long tensor in size of (H, W) in which each element is long integer  of [0,N] indicating belonging layer
+
+    '''
     if surfaceLabels is None:
         return None
-
+    #print(surfaceLabels.max(), surfaceLabels.min())
     H = height
     device = surfaceLabels.device
     N, W, D = surfaceLabels.shape  # N is the number of surface
     layerLabels = torch.zeros((H, W, D), dtype=torch.long, device=device)
     surfaceLabels = (surfaceLabels + 0.5).long()  # let surface height match grid
     surfaceCodes = torch.tensor(range(1, N + 1), device=device).unsqueeze(dim=-1).unsqueeze(dim=-1).expand_as(surfaceLabels)
+    #print(surfaceCodes.shape, surfaceLabels.shape)
     layerLabels.scatter_(0, surfaceLabels, surfaceCodes)  #surface 0 in its exact location marks as 1, surface N-1 mark as N.
-
+    
     for i in range(1,H):
         layerLabels[i,:, :] = torch.where(0 == layerLabels[i,:, :], layerLabels[i-1,:, :], layerLabels[i,:, :])
 
     return layerLabels
 
- 
-class sd_oct_flattenp_align(torch.utils.data.Dataset):
+class sd_oct_flatten(torch.utils.data.Dataset):
     """ 
         dataset for a2a_sd_oct.
+        the root should be "/data3/layer_segmentation/a2a_oct".
         the train and valid sets are splited by the label.csv.
     """
     
-    def __init__(self, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None, step=40):
-        super(sd_oct_flattenp_align, self).__init__()
+    def __init__(self, root, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None):
+        super(sd_oct_flatten, self).__init__()
 
         self.data_path = []
         self.data = []
         self.seg = []
         self.name = []
-        self.step = step
         self.usage = usage
         self.valid_fold = valid_fold
 
@@ -64,7 +73,81 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
             elif int(fold) != valid_fold and self.usage == "train":
                 self.data_path.append(path)
         print("load " + self.usage + " " + str(len(self.data_path)))
-        
+        maxi = 0
+        mini = 512
+        # load data and seg
+        for l in self.data_path:
+            cur_data = scio.loadmat(l)
+            cur_img = cur_data["data"][0,0]["flat_vol"][:, 300:700, 18:59]
+            cur_seg = cur_data["data"][0,0]["bds"][300:700, 18:59, :]
+            if cur_img.shape[0] != 320:
+                print(cur_img.shape[0])
+                continue
+            if sum(np.isnan(cur_seg)) > 0:
+                continue
+
+            if cur_seg.max() > maxi:
+                maxi = cur_seg.max()
+            if cur_seg.min() < mini:
+                mini = cur_seg.min()
+
+            
+            #cur_seg[:,:,2] = cur_seg[:,:,2] - cur_seg[:,:,1]
+            #cur_seg[:,:,1] = cur_seg[:,:,1] - cur_seg[:,:,0]
+            # normalization
+            mu = np.mean(cur_img, axis=0)
+            sigma = np.std(cur_img, axis=0)
+            cur_img = (cur_img - mu) / sigma
+            
+            self.data.append(cur_img.astype(np.float32))
+            self.seg.append(np.round(cur_seg).astype(np.float32))
+            self.name.append(l.split("/")[-1])
+        print(maxi, mini)
+        print("load " + self.usage + " " + str(len(self.data)))
+
+    def __getitem__(self, index):
+        patch = self.data[index]
+        seg = np.round(self.seg[index])
+
+        return torch.tensor(patch).permute(2,0,1), torch.tensor(seg).permute(1,2,0), self.name[index]
+    def __len__(self):
+        return len(self.data)
+ 
+class sd_oct_flattenp_align(torch.utils.data.Dataset):
+    """ 
+        dataset for a2a_sd_oct.
+        the root should be "/data3/layer_segmentation/a2a_oct".
+        the train and valid sets are splited by the label.csv.
+    """
+    
+    def __init__(self, root, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None, inte_dis = False,step=40, test = False):
+        super(sd_oct_flattenp_align, self).__init__()
+
+        self.data_path = []
+        self.data = []
+        self.seg = []
+        self.name = []
+        self.step = step
+        self.test = test 
+        self.inte_dis = inte_dis
+        self.usage = usage
+        self.valid_fold = valid_fold
+
+        # load file path and split train set and valid set.
+        label_file = "labelflattenedp.csv"
+        label_file_path = os.path.join(label_dir, label_file)
+        label = open(label_file_path, "r")
+        csv_reader = csv.reader(label)
+    
+        for path, fold in csv_reader:
+            path = path.replace("afterfp2","flattened")
+            if int(fold) == valid_fold and self.usage == "valid":
+                self.data_path.append(path)
+            elif int(fold) != valid_fold and self.usage == "train":
+                self.data_path.append(path)
+        print("load " + self.usage + " " + str(len(self.data_path)))
+        maxi = 0
+        mini = 512
         # load data and seg
         for l in self.data_path:
             try:
@@ -72,9 +155,12 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
             except:
                 print(l)
                 continue
-                
-            cur_img = cur_data["data"][0,0]["flat_vol"][:, 300:700, 18:58]
-            cur_seg = cur_data["data"][0,0]["bds"][300:700, 18:58, :]
+            if not self.test:
+                cur_img = cur_data["data"][0,0]["flat_vol"][:, 300:700, 18:59]
+                cur_seg = cur_data["data"][0,0]["bds"][300:700, 18:59, :]
+            else:
+                cur_img = cur_data["data"][0,0]["flat_vol"][:, 280:720, 18:59]
+                cur_seg = cur_data["data"][0,0]["bds"][280:720, 18:59, :]
             
             if cur_img.shape[0] != 320:
                 print(cur_img.shape[0])
@@ -83,7 +169,14 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
                 continue
             if cur_seg.min() < 5 or cur_seg.max() > 315:
                 continue
-
+            if cur_seg.max() > maxi:
+                maxi = cur_seg.max()
+            if cur_seg.min() < mini:
+                mini = cur_seg.min()
+            #cur_seg = cur_seg
+            
+            #cur_seg[:,:,2] = cur_seg[:,:,2] - cur_seg[:,:,1]
+            #cur_seg[:,:,1] = cur_seg[:,:,1] - cur_seg[:,:,0]
             # normalization
             mu = np.mean(cur_img, axis=0)
             sigma = np.std(cur_img, axis=0)
@@ -93,14 +186,152 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
             for i in range((cur_img.shape[1] - 48) // step):
                 cur_idx = i*step
                 self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+                #print(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
                 cur_idx = i*step
                 self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + 48,:])
                 self.seg.append(np.round(cur_seg).astype(np.float32)[cur_idx : cur_idx + 48,:,:])
+            #self.name.append(l.split("/")[-1]+str(i))
             cur_idx = 400 - 48
             self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+            #print(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
             self.data.append(cur_img.astype(np.float32)[:,cur_idx : cur_idx + 48,:])
             self.seg.append(np.round(cur_seg).astype(np.float32)[cur_idx : cur_idx + 48,:,:])
+            
+        #print(maxi, mini)
+        print("load " + self.usage + " " + str(len(self.data)))
+
+    def __getitem__(self, index):
+        patch = self.data[index]
+        name = self.name[index]
+        seg = self.seg[index]
+        #print(patch.shape, seg.shape)
+        
+        # data augmentation
+        
+        # if self.usage == "train":
+            # if np.random.random() > 0.7:
+                # patch = patch[:, ::-1, :].copy()
+                # seg = seg[::-1, :, :].copy()
                 
+        
+
+        layer_gt = getLayerLabels(torch.tensor(seg).permute(2,0,1), 320)
+        return torch.tensor(patch).unsqueeze(0), torch.tensor(seg).permute(2,0,1), layer_gt, name
+
+    def __len__(self):
+        return len(self.data)
+
+class hc_oct_noalign(torch.utils.data.Dataset):
+    def __init__(self, root, label_dir, data_name = "a2a_oct", valid_fold = 0, usage = "train", transform=None, random_patch = False, inter = 40, morechannel = False, patch_length=48):
+        super(hc_oct_noalign, self).__init__()
+        
+        self.random = random_patch
+        self.data_path = []
+        self.data = []
+        self.seg = []
+        self.name = []
+        self.inter = inter
+        self.usage = usage
+        self.valid_fold = valid_fold
+        self.morechannel = morechannel
+        
+        self.patch_length = patch_length
+        
+        # load file path and split train set and valid set.
+        label_file = "labelhc.csv"
+        label_file_path = os.path.join(label_dir, label_file)
+        label = open(label_file_path, "r")
+        csv_reader = csv.reader(label)
+        valid_fold = [0]
+        for path, fold_, fold in csv_reader:
+            if int(fold) in valid_fold and self.usage == "valid":
+                self.data_path.append(path)
+            elif int(fold) not in valid_fold and self.usage == "train":
+                self.data_path.append(path)
+        print("load " + self.usage + " " + str(len(self.data_path)))
+        self.random_patch()
+        
+    def random_patch(self):
+        self.data = []
+        self.seg = []
+        if self.usage == "train" and self.random:   
+            start_idx = np.random.randint(3,self.patch_length)
+        else:
+            start_idx = 0
+        print(start_idx , "_start")
+        
+        # load data and seg
+        for l in self.data_path:
+            try:
+                cur_data = scio.loadmat(l)
+            except:
+                print("load error")
+                continue
+                
+            cur_img = cur_data["data"][0,0]["flat_vol"]
+            cur_seg = cur_data["data"][0,0]["bds"]
+
+            if cur_img.shape[0] != 128:
+                print(cur_img.shape[0])
+                continue
+            if sum(np.isnan(cur_seg)) > 0:
+                continue
+                
+            # model inter
+            #cur_seg[:,:,2] = cur_seg[:,:,2] - cur_seg[:,:,1]
+            #cur_seg[:,:,1] = cur_seg[:,:,1] - cur_seg[:,:,0]
+            
+            # normalization
+            mu = np.mean(cur_img, axis=0)
+            sigma = np.std(cur_img, axis=0)
+            cur_img = (cur_img - mu) / sigma
+            
+            
+            cur_img = torch.tensor(cur_img.astype(np.float32))
+            cur_seg = torch.tensor(cur_seg.astype(np.float32)).permute(2,0,1)
+            
+            if self.morechannel:
+                # YufanHe uses x,y index as extra channel.
+                H = cur_img.shape[0]
+                W = cur_img.shape[1]
+                
+                X = torch.arange(W).view((1, W)).unsqueeze(2).expand(cur_img.size()).float().unsqueeze(dim=0)/(W*1.0)
+                Y = torch.arange(H).view((H, 1)).unsqueeze(2).expand(cur_img.size()).float().unsqueeze(dim=0)/(H*1.0)
+                
+                cur_img = torch.cat((cur_img.unsqueeze(0), Y, X), dim=0)
+            else:
+                cur_img = cur_img.unsqueeze(0)
+            
+            #print(cur_img.shape, cur_seg.shape)
+            if self.usage == "valid":
+                step = self.inter
+                start_idx = 0
+                start = 0
+                end = 1024
+            else:
+                cur_seg = torch.round(cur_seg)
+                step = self.inter
+                start = 2
+                end = 1021
+            for i in range(start, (end - self.patch_length - start_idx) // step):  
+            
+                cur_idx = i*step  + start_idx
+
+                cur_i = cur_img[:,:,cur_idx:cur_idx + self.patch_length,:]
+                cur_s = cur_seg[:,cur_idx:cur_idx + self.patch_length,:]
+                
+                self.data.append(cur_i)
+                self.seg.append((cur_s))
+                self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+
+            cur_idx = end - self.patch_length
+
+            cur_i = cur_img[:,:,cur_idx:cur_idx + self.patch_length,:]
+            cur_s = cur_seg[:,cur_idx:cur_idx + self.patch_length,:]
+            self.data.append(cur_i)
+            self.seg.append((cur_s))
+            self.name.append(l.split("/")[-1]+str(i).zfill(3)+str(cur_idx).zfill(3))
+                   
         print("load " + self.usage + " " + str(len(self.data)))
 
     def __getitem__(self, index):
@@ -108,12 +339,20 @@ class sd_oct_flattenp_align(torch.utils.data.Dataset):
         name = self.name[index]
         seg = self.seg[index]
         
-        layer_gt = getLayerLabels(torch.tensor(seg).permute(2,0,1), 320)
-        return torch.tensor(patch).unsqueeze(0), torch.tensor(seg).permute(2,0,1), layer_gt, name
+        # data augmentation
+        '''
+        if self.usage == "train":
+            if np.random.random() > 0.7:
+                patch = torch.flip( patch, dims = [1])
+                seg = torch.flip(seg, dims = [0])
+        '''        
+        layer_gt = getLayerLabels(seg, 128)
+        
+        return patch, seg, layer_gt, name
 
     def __len__(self):
         return len(self.data)
-
+  
 class sd_oct_flatten_ori_align_doe(torch.utils.data.Dataset):
     """ 
         dataset for a2a_sd_oct.
@@ -427,7 +666,3 @@ class sd_oct_flatten_ori_align_doe_lesion(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.data)
-        
-
-
-        
